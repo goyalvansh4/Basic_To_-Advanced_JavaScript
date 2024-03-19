922e85e2e5bdda0de6efd56be0367bec3edfc043
Vansh Gupta <97226346+goyalvansh4@users.noreply.github.com>
2024-03-19 04:58:02 +0000

    "learn data validation"

--- MongoDB/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1/testDatabase')
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...', err));
